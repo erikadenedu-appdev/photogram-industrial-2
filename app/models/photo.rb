2414aae799ce04ab843998724a9e12bcb977f18d
erikadenedu <eaden@chicagobooth.edu>
2021-04-21 17:32:12 +0000

    Added_counter_caches

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -12,5 +12,5 @@
 #  updated_at     :datetime         not null
 #
 class Photo < ApplicationRecord
-  belongs_to :owner, class_name: "User"
+  belongs_to :owner, class_name: "User", :counter_cache: true
 end
