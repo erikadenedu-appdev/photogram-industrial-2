cc639f50e0f241dd9bb51978f6074ef6e73401b7
erikadenedu <eaden@chicagobooth.edu>
2021-04-21 18:26:31 +0000

    add_ind_assoc

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -13,6 +13,8 @@
 #
 class Photo < ApplicationRecord
   belongs_to :owner, class_name: "User", :counter_cache: true
-  has_many :comments, foreign_key: :photo_id
-  has_many :likes, foreign_key: :photo_id
+  has_many :comments
+  has_many :likes
+
+  has_many :fans, through: :likes#, source: :fan
 end
